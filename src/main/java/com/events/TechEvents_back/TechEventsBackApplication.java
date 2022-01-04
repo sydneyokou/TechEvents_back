372368097f542610e93e8d62d0d2325b43e8645a
OKOU <sydney.okou@capgemini.com>
2022-01-04 22:43:18 +0100

    docker-compose file creation and database connection in .properties

---
+++ src/main/java/com/events/TechEvents_back/TechEventsBackApplication.java
@@ -0,0 +1,13 @@
+package com.events.TechEvents_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechEventsBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechEventsBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/events/TechEvents_back/TechEventsBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.events.TechEvents_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechEventsBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
